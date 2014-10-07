50711a11adc193884b252a037c241d559440b1b5
Leeroy Brun <leeroy.brun@gmail.com>
2014-10-07 18:44:50 +0200

    Add examples to config file

--- puppet/manifests/config.pp
+++ puppet/manifests/config.pp
@@ -1,20 +1,20 @@
 #---------------------------------------------------------------------
 # Config
 #---------------------------------------------------------------------
-$reportEmail 		 = 'REPORT_EMAIL'
+$reportEmail 		 = 'REPORT_EMAIL'			# Ex: email@domain.com
 
 $rootPwd     		 = 'ROOT_PWD_ENCODED'
-$newUserName 		 = 'USER_NAME'
+$newUserName 		 = 'USER_NAME'				# Ex: myUserName
 $newUserPwd  		 = 'USER_PWD_ENCODED'
 
-$sshKeyComment 		 = 'SSH_KEY_COMMENT'
-$sshKeyType    		 = 'SSH_KEY_TYPE'
-$sshKeyContent 		 = 'SSH_KEY_CONTENT'
-$sshPort	   		 = SSH_PORT
+$sshKeyComment 		 = 'SSH_KEY_COMMENT'		# Ex: nick@magpie.puppetlabs.lan
+$sshKeyType    		 = 'SSH_KEY_TYPE'			# Ex: ssh-rsa
+$sshKeyContent 		 = 'SSH_KEY_CONTENT'		# Ex: AAAAB3NzaC1yc2E[...]3YhrFwjtUqXfdaQ==
+$sshPort	   		 = SSH_PORT					# Ex: 22
 
 # Tripwire
 $twLocalPassphrase 	 = 'TW_LOCAL_PASSPHRASE'
 $twSitePassphrase 	 = 'TW_SITE_PASSPHRASE'
 
-$knockdSequenceOpen  = 'KNOCKD_SEQ_OPEN' #4000:udp,4697:tcp,3102:udp
-$knockdSequenceClose = 'KNOCKD_SEQ_CLOSE' #7634:tcp,3861:udp,4923:tcp
+$knockdSequenceOpen  = 'KNOCKD_SEQ_OPEN' 		# Ex: 4000:udp,4697:tcp,3102:udp
+$knockdSequenceClose = 'KNOCKD_SEQ_CLOSE' 		# Ex: 7634:tcp,3861:udp,4923:tcp
