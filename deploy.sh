2f810016d1d37c99a95ba77b8dd07a77a0e47c2c
Leeroy Brun <leeroy.brun@gmail.com>
2014-10-08 21:41:53 +0200

    Remove Stackscripts params from bash deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-# 
-# We define parameters the user should provide when deploying to Linode with Stackscripts.
-# 
-#<UDF name="REPORT_EMAIL" Label="Report email" default="root@localhost" />
-#<UDF name="ROOT_PWD" Label="Root password" default="" />
-#<UDF name="USER_NAME" Label="New (non-root) user name" default="myUser" />
-#<UDF name="USER_PWD" Label="New (non-root) user password" default="" />
-#<UDF name="SSH_KEY_COMMENT" Label="SSH key comment" default="" />
-#<UDF name="SSH_KEY_TYPE" Label="SSH key type" default="ssh-rsa" />
-#<UDF name="SSH_KEY_CONTENT" Label="SSH key content" default="" />
-#<UDF name="SSH_PORT" Label="SSH port" default="" />
-#<UDF name="TW_LOCAL_PASSPHRASE" Label="Tripwire local passphrase" default="" />
-#<UDF name="TW_SITE_PASSPHRASE" Label="Tripwire site passphrase" default="" />
-#<UDF name="KNOCKD_SEQ_OPEN" Label="Knockd sequence open" default="" />
-#<UDF name="KNOCKD_SEQ_CLOSE" Label="Knockd sequence close" default="" />
-#
 
 exec > >(tee /root/deploy.log)
 exec 2>&1
