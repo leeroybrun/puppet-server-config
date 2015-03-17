864c18d3fe02693d71cd65cf1b3237cf4ea7c6f6
Leeroy Brun <leeroy.brun@gmail.com>
2015-03-17 09:25:44 +0100

    Update config.example.pp

--- puppet/manifests/config.example.pp
+++ puppet/manifests/config.example.pp
@@ -3,7 +3,6 @@
 #---------------------------------------------------------------------
 $reportEmail 		 = 'REPORT_EMAIL'			# Ex: email@domain.com
 
-$rootPwd     		 = 'ROOT_PWD_HASHED'
 $newUserName 		 = 'USER_NAME'				# Ex: myUserName
 $newUserPwd  		 = 'USER_PWD_HASHED'
 
@@ -13,8 +12,8 @@ $sshKeyContent 		 = 'SSH_KEY_CONTENT'		# Ex: AAAAB3NzaC1yc2E[...]3YhrFwjtUqXfdaQ
 $sshPort	   		 = SSH_PORT					# Ex: 22
 
 # Tripwire
-$twLocalPassphrase 	 = 'TW_LOCAL_PASSPHRASE'
-$twSitePassphrase 	 = 'TW_SITE_PASSPHRASE'
+#$twLocalPassphrase 	 = 'TW_LOCAL_PASSPHRASE'
+#$twSitePassphrase 	 = 'TW_SITE_PASSPHRASE'
 
 $knockdSequenceOpen  = 'KNOCKD_SEQ_OPEN' 		# Ex: 4000:udp,4697:tcp,3102:udp
 $knockdSequenceClose = 'KNOCKD_SEQ_CLOSE' 		# Ex: 7634:tcp,3861:udp,4923:tcp
